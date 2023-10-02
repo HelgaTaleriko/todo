6f9642027599e26d0a0fefc07f3918872c3374ca
HelgaTaleriko <89998178494@mail.ru>
2023-10-03 02:53:49 +0300

    разбила предварительно на компоненты 1.3.13

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import TaskList from "./components/TaskList/TaskList";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <section className="todoapp">
+            <Header/>
+
+            <section className="main">
+                <TaskList/>
+                <Footer/>
+            </section>
+        </section>
+);
 }
 
 export default App;
---
+++ src/components/TaskList/Task/TaskButton/TaskDestroy.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const TaskDestroy =()=>{
+    return(
+        <button className="icon icon-destroy"></button>
+    )
+}
+export default TaskDestroy
